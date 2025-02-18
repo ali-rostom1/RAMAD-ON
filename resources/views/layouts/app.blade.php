954b1ea914635127228757b90f4c83ecc6fa4d6f
ali-rostom1 <ali.rostom220@gmail.com>
2025-02-18 17:22:17 +0100

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     </head>
     <body class="bg-gradient-to-b from-amber-50 to-white min-h-screen">
     @include('comps.header')
